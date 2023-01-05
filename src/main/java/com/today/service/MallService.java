b47c57e95493ff9675f70faba05dcdb063a545c5
ArfyB <121779026+ArfyB@users.noreply.github.com>
2023-01-05 16:35:30 +0900

    contect

--- src/main/java/com/today/service/MallService.java
+++ src/main/java/com/today/service/MallService.java
@@ -1,9 +1,23 @@
 package com.today.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.today.mapper.CategoryMapper;
+import com.today.vo.Category;
 
 @Service
 public class MallService 
 {
+	@Autowired
+	public CategoryMapper mapper;
 	
+	public List<Category> list()
+	{
+		List<Category> list = mapper.list();
+		
+		return list;
+	}
 }
