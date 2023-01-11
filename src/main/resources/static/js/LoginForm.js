864b737fad81ae01518d8e94321e6d33dd641342
ArfyB <121779026+ArfyB@users.noreply.github.com>
2023-01-11 17:00:52 +0900

    123

--- src/main/resources/static/js/LoginForm.js
+++ src/main/resources/static/js/LoginForm.js
@@ -1,23 +1,24 @@
-const signUpBox = document.querySelector(".sign-up-box");
+const signUpBox = document.querySelector(".sign-up-box");	//.sign-up-box 클래스를 signUpBox라고 부르자
 
-signUpBox.addEventListener("click", handleClick, false);
+signUpBox.addEventListener("click", handleClick, false); // 
 function handleClick(event) {
-  if (signUpBox.classList.contains("active")) return;
+  if (signUpBox.classList.contains("active")) return;	//active클래스 존재여부
   let str = `
         <span onclick="event.stopPropagation(); removedActive();">X</span>
-        <input type="text" name="name" placeholder="아이디"/>
-        <input type="password" name="password" placeholder="Password"/>
-        <input type="email" name="email" placeholder="Email"/>
-        <input type="email" name="email" placeholder="Email"/>
+        <input type="text" name="name" placeholder="아이디" id="userid"/>
+        <input type="password" name="password" placeholder="Password" id="pwd"/>
+        <input type="text" placeholder="닉네임" id='nickname'/>
+        <input type="email" name="email" placeholder="Email" id="email"/>
+        <input type="email" name="email1" placeholder="Email" id="email1"/>
         <button onclick="event.stopPropagation(); handleSignUp();" >가입완료</button>
-    `;
-  this.classList.toggle("active");
+    `;						// stopPropagation = 상위 엘리먼트로 이벤트전파 중지
+  this.classList.toggle("active"); // 클래스명 스위치
   this.innerHTML = "";
   setTimeout(() => (this.innerHTML = str), 500);
 }
 
 function removedActive() {
-  signUpBox.classList.remove("active");
+  signUpBox.classList.remove("active");	// 클래스명 제거
   signUpBox.innerHTML = `<i class="material-icons">+</i>`;
 }
 function handleSignUp() {
@@ -31,7 +32,14 @@ function handleSignUp() {
     emailField.value === "" ||
     passwordField.value === ""
   ) {
+	alert("모든 항목은 필수 입력 입니다");
     return;
   }
+  else {
+	//var userid = document.getElementById("userid").value; html에서 userid
+	alert(userdata);
+}
   removedActive();
 }
+
+// loginbtn.addEventListener("click", function(event){alert('Hello');});
